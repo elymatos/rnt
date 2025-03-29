5f7336f01b54485e80a3849beaf53290e9ef1477
Ely Edison Matos <ely.matos@gmail.com>
2025-03-28 22:18:40 -0300

    Quartz sync: Mar 28, 2025, 10:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "elymatos.github.io/rnt/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
